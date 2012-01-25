cbae050a9d87ee99af617c6938c22bba7efd907c
Philip Cali <philip.cali@gmail.com>
2012-01-25 09:09:50 -0600

    Pruning CPS information

---
+++ lang/en/block_ues_meta_viewer.php
@@ -0,0 +1,17 @@
+<?php
+
+$string['pluginname'] = 'Course Preferences';
+
+$string['viewer'] = 'User Data Viewer';
+$string['not_available'] = 'NA';
+$string['no_results'] = 'No Results.';
+$string['found_results'] = 'Found: ';
+
+// String to be moved into CPS
+$string['user_keypadid'] = 'Keypad ID';
+$string['user_major'] = 'Major';
+$string['user_college'] = 'College';
+$string['user_year'] = 'Year';
+$string['user_reg_status'] = 'Registration';
+$string['user_degree'] = 'Graduating';
+$string['user_ferpa'] = 'Ferpa';
