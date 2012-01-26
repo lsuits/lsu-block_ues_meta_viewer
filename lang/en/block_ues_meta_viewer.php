10a2221be588a7f166c3a73907b3a5c5f788b8ba
Philip Cali <philip.cali@gmail.com>
2012-01-26 13:35:20 -0600

    Removes unused strings

--- lang/en/block_ues_meta_viewer.php
+++ lang/en/block_ues_meta_viewer.php
@@ -17,12 +17,3 @@ $string['no_results'] = 'No Results.';
 $string['found_results'] = 'Found: ';
 
 $string['ues_meta_viewer:access'] = 'Access the Meta viewer';
-
-// String to be moved into CPS
-$string['user_keypadid'] = 'Keypad ID';
-$string['user_major'] = 'Major';
-$string['user_college'] = 'College';
-$string['user_year'] = 'Year';
-$string['user_reg_status'] = 'Registration';
-$string['user_degree'] = 'Graduating';
-$string['user_ferpa'] = 'Ferpa';
